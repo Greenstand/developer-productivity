1da10d52730cf2545a7be6ca176744c90811ae36
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 18:57:56 -0400

    Adding newdb

--- mainSetup.tf
+++ mainSetup.tf
@@ -6,7 +6,7 @@ module "databaseCluster" {
   cluster_size    = "db-s-1vcpu-1gb"
   cluster_version = "11"
   cluster_nodecount  = 1
-  database_name   = random_pet.id
+  database_name   = random_pet.name.id
   do_token        = var.do_token
   project_name    = "Development"
 }
