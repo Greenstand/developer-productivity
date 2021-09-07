334edc153d2b0ecb197efc0971ba0c481c9c5b68
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 15:42:22 -0400

    Testing with Apply tf plan

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ terraform {
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      # version = "~> 2.0"
     }
   }
 }
 
-# provider digitalocean{
-#     token = ${var.do_token}
-# }
-
-data "digitalocean_account" "account_info" {}
-
-output "droplet_limit" {
-  value = data.digitalocean_account.account_info.droplet_limit
+provider digitalocean{
+    token = var.do_token
 }
+
+# data "digitalocean_account" "account_info" {}
+
+# output "droplet_limit" {
+#   value = data.digitalocean_account.account_info.droplet_limit
+# }
