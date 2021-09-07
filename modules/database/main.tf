59989568900d42a16c4748c69b4c35e91623c8a8
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 16:14:18 -0400

    Adding tf modules structure

---
+++ modules/database/main.tf
@@ -0,0 +1,28 @@
+
+data "digitalocean_project" "development"{
+  name = var.project_name
+}
+
+resource "digitalocean_database_db" "database" {
+  # count = 1
+  cluster_id = digitalocean_database_cluster.cluster.id
+  name       = var.database_name
+}
+
+
+resource "digitalocean_database_cluster" "cluster" {
+  name       = var.cluster_name
+  engine     = var.cluster_engine
+  version    = var.cluster_version
+  size       = var.cluster_size
+  region     = var.cluster_region
+  node_count = var.cluster_nodecount
+}
+
+
+resource "digitalocean_project_resources" "barfoo" {
+  project = data.digitalocean_project.development.id
+  resources = [
+    digitalocean_database_cluster.cluster.urn
+  ]
+}
