ae361fd1c075956b6b0f0873ecaeef98516f6676
Ximena <ximenandrea.ro@gmail.com>
2021-09-14 19:31:11 -0400

    Adding a new db cluster

--- mainSetup.tf
+++ mainSetup.tf
@@ -1,12 +1,12 @@
 module "databaseCluster" {
   source          = "./modules/databaseCluster"
-  cluster_name    = "cluster1"
+  cluster_name    = "cluster"
   cluster_engine  = "pg"
   cluster_region  = "sfo2"
   cluster_size    = "db-s-1vcpu-1gb"
   cluster_version = "11"
   cluster_nodecount  = 1
-  database_name   = "ximena"
+  database_name   = "test"
   do_token        = var.do_token
   project_name    = "Development"
 }
